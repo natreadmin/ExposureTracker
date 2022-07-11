22c120d06ab076837e6874cfe9d9dacaaf5d45d0
natreadmin <vic.platon@nat-re.com>
2022-07-11 19:11:05 +0800

    Added Riders in Model

--- Models/TranslationTables.cs
+++ Models/TranslationTables.cs
@@ -3,19 +3,25 @@
     public class TranslationTables
     {
         [Key]
-        public string plan_code { get; set; }
+        public int id { get; set; }
         [Required]
 
-        public string ceding_company { get; set; }
+        public string identifier { get; set; }
 
-        public string cedant_code { get; set; }
+        public string plan_code { get; set; }
 
-        public string benefit_cover { get; set; }
+        public string? ceding_company { get; set; }
 
-        public string insured_prod { get; set; }
+        public string? cedant_code { get; set; }
 
-        public string prod_description { get; set; }
+        public string? benefit_cover { get; set; }
 
-        public string base_rider { get; set; }
+        public string? insured_prod { get; set; }
+
+        public string? prod_description { get; set; }
+
+        public string? base_rider { get; set; }
     }
+
 }
+   
